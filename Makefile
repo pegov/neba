109582b9a37afb781d3aa166244d90a1f9751388
pegov <dvpegov@gmail.com>
2025-01-06 23:38:55 +0300

    draw circle

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 build:
 	@mkdir -p build/
-	g++ -Ivendor/include -Lvendor/lib -o build/neba src/main.cpp -lraylib
+	g++ -Ivendor/include -fuse-ld=mold -Lvendor/lib -o build/neba src/main.cpp -lraylib
 
 run:
 	@./build/neba
--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,21 @@ int main() {
     InitWindow(WIDTH, HEIGHT, "neba example");
     SetTargetFPS(144);
 
+    Vector2 circle = {100.0f, 100.0f};
+    float speed = 200.0f;
+
     while (!WindowShouldClose()) {
+        float dt = GetFrameTime();
+
+        if (IsKeyDown(KEY_W)) circle.y -= speed*dt;
+        if (IsKeyDown(KEY_S)) circle.y += speed*dt;
+        if (IsKeyDown(KEY_A)) circle.x -= speed*dt;
+        if (IsKeyDown(KEY_D)) circle.x += speed*dt;
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawText("neba text", 200, 200, 20, LIGHTGRAY);
+            DrawCircleV(circle, 10.0f, RED);
         EndDrawing();
     }
 
