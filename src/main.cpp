f3bd62f6f04b2a08367ac224cc6d83fbfec91658
pegov <dvpegov@gmail.com>
2025-01-07 22:34:17 +0300

    add projectiles

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+
 #include <rl/raylib.h>
 #include <rl/raymath.h>
 
 const int WIDTH = 640;
 const int HEIGHT = 480;
+
+struct Bullet {
+    Vector2 start;
+    Vector2 pos;
+    Vector2 direction;
+    bool off = false;
+};
 
 int main() {
     InitWindow(WIDTH, HEIGHT, "neba example");
@@ -12,6 +23,13 @@ int main() {
     float speed = 250.0f;
     float acc_speed = 0.01f;
 
+    float bullet_speed = 100.0f;
+    float bullet_limit = 100.0f;
+    std::vector<Bullet> bullets;
+    bullets.reserve(256);
+
+    bullets.push_back({{circle.x + 1.0f, circle.y + 1.0f}, {circle.x + 1.0f, circle.y + 1.0f}, {1.0f, 1.0f}});
+
     Vector2 mov = {0.0f, 0.0f};
     Vector2 acc = {0.0f, 0.0f};
     Vector2 slow = {0.0f, 0.0f};
@@ -19,7 +37,9 @@ int main() {
 
     while (!WindowShouldClose()) {
         float dt = GetFrameTime();
+        Vector2 mouse = GetMousePosition();
 
+        // TODO: lerp
         if (IsKeyDown(KEY_W)) acc.y -= acc_speed;
         if (IsKeyDown(KEY_S)) acc.y += acc_speed;
         if (IsKeyDown(KEY_A)) acc.x -= acc_speed;
@@ -49,6 +69,7 @@ int main() {
             acc.x += neg.x * acc_speed;
         }
 
+
         Vector2 norm = Vector2Normalize(acc);
         float dist = Vector2Length(acc);
         if (dist > 1.0f) {
@@ -57,10 +78,39 @@ int main() {
 
         circle += norm * dist * speed * dt;
 
+        for (auto &bullet : bullets) {
+            if (bullet.off) {
+                continue;
+            }
+
+            bullet.pos += bullet.direction * bullet_speed * dt;
+            printf("bullet x=%f y=%f\n", bullet.pos.x, bullet.pos.y);
+
+            if (Vector2Distance(bullet.pos, bullet.start) > bullet_limit) {
+                bullet.off = true;
+            }
+        }
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            bullets.push_back({
+                circle,
+                circle,
+                Vector2Normalize(mouse - circle),
+            });
+        }
+
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawText("neba text", 200, 200, 20, LIGHTGRAY);
             DrawCircleV(circle, 10.0f, RED);
+            DrawLineEx(circle, mouse, 1.0f, BLACK);
+
+            for (auto &bullet : bullets) {
+                if (!bullet.off) {
+                    DrawCircleV(bullet.pos, 3.0f, BLACK);
+                }
+            }
         EndDrawing();
     }
 
